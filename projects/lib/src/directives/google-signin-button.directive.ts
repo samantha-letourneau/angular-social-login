54e35343b1d336bef657546ba7ccff3ddf702f9b
samantha-letourneau <samantha.letourneau@outlook.com>
2022-07-15 21:54:27 -0400

    Add google rendering options

--- projects/lib/src/directives/google-signin-button.directive.ts
+++ projects/lib/src/directives/google-signin-button.directive.ts
@@ -1,20 +1,31 @@
-import { Directive, ElementRef} from '@angular/core';
+import { Directive, ElementRef, Input} from '@angular/core';
 import { take } from 'rxjs';
 import { SocialAuthService } from '../socialauth.service';
+import { Type, Shape, Theme, Label, Size } from '../google-rendering-option';
 
 @Directive({
-  // eslint-disable-next-line @angular-eslint/directive-selector
-  selector: 'asl-google-signin-button',
+  selector: 'google-signin-button',
 })
 export class GoogleSigninButtonDirective {
+  @Input(Type) type!: Type;
+  @Input(Shape) shape?: Shape;
+  @Input(Theme) theme?: Theme;
+  @Input(Label) label: Label;
+  @Input(Size) size: Size;
+  @Input('width') width: number;
   constructor(
     el: ElementRef,
     socialAuthService: SocialAuthService
   ) {
+    
     socialAuthService.initState.pipe(take(1)).subscribe(() => {
       google.accounts.id.renderButton(el.nativeElement, {
-        type: 'icon',
-        size: 'medium',
+        type: this.type === 1 ? 'standard' : 'icon',
+        size: this.size === 1 ? 'small' : this.size === 2 ? 'medium' : 'large',
+        theme: this.theme === 1 ? 'outline' : this.theme === 2 ? 'filled_blue' : 'filled_black',
+        label: this.label === 1 ? 'signin_with' : this.label === 2 ? 'signin_with' : this.label === 3 ? 'continue_with' : 'sign_in',
+        shape: this.shape === 1 ? 'rectangular' : this.shape === 2 ? 'pill' : this.shape === 3 ? 'square' : 'circle',
+        width: this.width
       });
     })
   }
---
+++ projects/lib/src/google-rendering-option.ts
@@ -0,0 +1,34 @@
+export enum Type {
+    Standard = 1,
+    Icon = 2
+}
+
+export enum Shape {
+    /** For standard shape type */
+    Rectangular = 1,
+    /** For standard shape type */
+    Pill = 2,
+    /** For icon shape type */
+    Square = 3,
+    /** For icon shape type */
+    Circle = 4
+}
+
+export enum Size {
+    Small = 1,
+    Medium = 2,
+    Large = 3
+}
+
+export enum Theme {
+    Outline = 1,
+    FilledBlue = 2,
+    FilledBlack = 3
+}
+
+export enum Label {
+    SigninWith = 1,
+    SignupWith = 2,
+    ContinueWith = 3,
+    Signin = 4
+}
