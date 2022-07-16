8229594f82813fac11740cdc584c446722143481
samantha-letourneau <samantha.letourneau@outlook.com>
2022-07-15 23:47:43 -0400

    eslint disabled for google directive

--- projects/lib/src/directives/google-signin-button.directive.ts
+++ projects/lib/src/directives/google-signin-button.directive.ts
@@ -4,15 +4,16 @@ import { SocialAuthService } from '../socialauth.service';
 import { Type, Shape, Theme, Label, Size } from '../google-rendering-option';
 
 @Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
   selector: '[google-signin-button]'
 })
 export class GoogleSigninButtonDirective {
-  @Input("type") type!: Type;
-  @Input("shape") shape?: Shape;
-  @Input("theme") theme?: Theme;
-  @Input("label") label: Label;
-  @Input("size") size: Size;
-  @Input('width') width: string;
+  @Input() type!: Type;
+  @Input() shape?: Shape;
+  @Input() theme?: Theme;
+  @Input() label: Label;
+  @Input() size: Size;
+  @Input() width: string;
   constructor(
     el: ElementRef,
     socialAuthService: SocialAuthService
